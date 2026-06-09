96ee59b69cb3040b6c9b504ffe3c7c100ac185b3
Phi Lam <lamyenphi14@gmail.com>
2026-06-09 21:01:21 +0800

    Add spdlog as submodule and use it

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,10 @@
 #include <thread>
+#include <format>
+#include <spdlog/async.h>
+#include <spdlog/spdlog.h>
+#include <spdlog/sinks/rotating_file_sink.h>
 
+#include "utils/TimeUtils.h"
 #include "ibkr/IbkrClient.h"
 #include "market_data/MarketDataEngine.h"
 
@@ -8,36 +13,52 @@ const unsigned SLEEP_TIME   = 3;
 
 int main()
 {
+    // Initialize market data engine and client
     MarketDataEngine marketDataEngine;
     IbkrClient       client(marketDataEngine);
     const char      *host     = "127.0.0.1";
     int              port     = 7497;
     int              clientId = 0;
 
+    // Initialize time ET
+    TimeUtils::init();
+
     unsigned attempt = 0;
+
+    // Initialize logger
+    std::string log_path = std::format("logs/orion_{}.log", getCurrentDate());
+
+    // Start SPD logger
+    auto logger = spdlog::create_async<spdlog::sinks::rotating_file_sink_mt>(
+        "orion", log_path, 1024 * 1024 * 10, 3); // mutex but not on hot path, runs async
+    spdlog::set_default_logger(logger);
 
     for (;;)
     {
         ++attempt;
+        spdlog::info(
+            "Connection attempt {}/{} | client: {}, host: {}, port: {}", attempt, MAX_ATTEMPTS,
+            clientId, host, port);
 
         client.connect(host, port, clientId);
 
+        if (attempt >= MAX_ATTEMPTS)
+        {
+            spdlog::warn("Max connection attempts {} reached, ending session", MAX_ATTEMPTS);
+            break;
+        }
+
+        // Main connection
         while (client.isConnected())
         {
             client.run();
         }
 
-        if (attempt > MAX_ATTEMPTS)
-        {
-            printf("Max connection attempts #%d ran, ending session\n", MAX_ATTEMPTS);
-            break;
-        }
-
-        printf("Sleeping %u seconds before next attempt\n", SLEEP_TIME);
+        spdlog::info("Sleeping {} seconds before next attempt", SLEEP_TIME);
         std::this_thread::sleep_for(std::chrono::seconds(SLEEP_TIME));
     }
 
-    printf("End of Session\n");
+    spdlog::info("End of Session");
 
     return 0;
 }
